3b49aec1ea1d3e277351f23c2f8e7fc4f92eae61
Shivansh Srivastava <srishivansh2019@gmail.com>
2025-06-24 15:29:50 +0530

    updatedDescription

--- ProductsMock/Models/Product.cs
+++ ProductsMock/Models/Product.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty; // newly added
         public string? ImageUrl { get; set; }
     }
 }
